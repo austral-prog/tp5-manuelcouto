2e52688d4808cc8a510644c1d3baf9f3ab9c882a
Manuel Couto <mcouto1@mail.austral.edu.ar>
2025-09-26 18:06:42 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,9 +1,20 @@
-# Replace the "ANSWER HERE" for your answer
 def max_of_two(x, y):
-    resu1=max(x,y)
+    resu1=0
+    if x>y:
+        resu1=x
+    else:
+        resu1=y
     return resu1
 
 
 def max_of_three(x, y, z):
-    resu2=max(x,y,z)
+    if x>y:
+        if x>z:
+            resu2=x
+        else:
+            resu2=z
+    elif y>z:
+        resu2=y
+    else:
+        resu2=z
     return resu2
